be315fb2eae4bafeaa6be671da972b021742d039
Osman M <115216994+osmanmurat@users.noreply.github.com>
2022-10-24 01:17:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ x_boyut=500
 foto = cv2.resize(cv2.imread("kkk.jpg",0),(x_boyut,y_boyut))
 
 cv2.imshow("foto",foto)
-cv2.waitKey()
+#cv2.waitKey()
 
 hist = np.zeros(256,dtype = int);
 for f in range (x_boyut):
