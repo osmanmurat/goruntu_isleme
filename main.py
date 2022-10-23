9257f0bbc63c534cc183c1639585e7aa1bd79473
osmanmurat <02210201213@ogr.inonu.edu.tr>
2022-10-24 01:08:52 +0300

    k

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as ml
+
+y_boyut=500
+x_boyut=500
+foto = cv2.resize(cv2.imread("kkk.jpg",0),(x_boyut,y_boyut))
+
+cv2.imshow("foto",foto)
+cv2.waitKey()
+
+hist = np.zeros(256,dtype = int);
+for f in range (x_boyut):
+    for k in range (y_boyut):
+        hist[foto[f,k]] = hist[foto[f,k]] + 1
+
+ml.plot(hist)
+ml.show();
